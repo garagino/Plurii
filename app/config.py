04db2ee4dc51b58d9350e672dd65f5f156dec315
Francisco Antonio da Luz Filho <falf@cesar.school>
2023-11-06 14:26:31 -0300

    Teste

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 from pydantic import BaseSettings
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
 
 
 class Settings(BaseSettings):
@@ -14,4 +17,29 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+class AsyncDatabaseSession:
+
+    def __init__(self) -> None:
+        self.session = None
+        self.engine = None
+
+    def __getattr__(self,name):
+        return getattr(self.session,name)
+
+
+    async def create_all(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
+
+
+db = AsyncDatabaseSession()
+
+async def commit_rollback():
+    try:
+        await db.commit()
+    except Exception:
+        await db.rollback()
+        raise
 
--- app/main.py
+++ app/main.py
@@ -1,11 +1,48 @@
 from fastapi import FastAPI
 from app import models
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
+from app.config import db
 
-app = FastAPI()
 
 
 models.Base.metadata.create_all(bind=engine)
+
+
+origins= [
+    "http://localhost:3000"
+]
+
+def init_app():
+    db.init()
+
+    app = FastAPI(
+        title= "Garagino App",
+        description= "Login Page",
+        version= "1"
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+
+    @app.on_event("startup")
+    async def starup():
+        await db.create_all()
+    
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.close()
+
+    return app
+
+app = init_app()
+
+
 
 
 @app.get("/")

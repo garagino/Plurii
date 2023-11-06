3dbe85c86adaadb13ebe39b3e17d4a7bb4578ca3
Francisco Antonio da Luz Filho <falf@cesar.school>
2023-11-06 08:32:16 -0300

    Resolução do problema com psycopg2

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 from fastapi_utils.guid_type import setup_guids_postgresql
 
-POSTGRES_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
+POSTGRES_URL = f"postgresql+pg8000://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
 
 engine = create_engine(
     POSTGRES_URL, echo=True

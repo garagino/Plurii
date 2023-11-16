0762e9e6afdcc4dc7213df2f013b58da8a13ef08
Francisco Antonio da Luz Filho <falf@cesar.school>
2023-11-16 10:35:54 -0300

    att alembic

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,8 @@ from alembic import context
 
 from app.models import user, labroom
 
+from app.database import POSTGRES_URL
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -15,6 +17,8 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config.set_main_option("sqlalchemy.url", POSTGRES_URL)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
